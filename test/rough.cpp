117ef04039cfae0f0ec7a6f318c51b73cf15404f
Manjeet raj <32019813+ManjeetRaj@users.noreply.github.com>
2021-02-05 01:18:20 +0530

    Update rough.cpp

--- test/rough.cpp
+++ test/rough.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 
 using namespace std;
-
-int main()
+void fun(const u_char *p)
 {
-    const u_char chr[8] = "tesu123";
-    const u_int* p = chr;
-    cout<<"p: "<<*(p-1);
+    const u_char* chr = p;
+    cout<<"p: "<<*(p-1)<<endl;
     if(*(p-2)=='t')
     {
         cout<<"IN: IF"<<endl;
         
     }
     cout<<"OUT: IF"<<endl;
+}
+
+int main()
+{
+    const u_char chr[8] = "tesu123";
+    fun(chr);
+    cout<<"IN: MAIN"<<endl;
     return 0;
 }
